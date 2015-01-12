f5c3250d6d96ed32c673d0e2cc14e6d02a94f35a
akiselicki <a.kiselicki@levi9.com>
2015-01-12 11:46:12 +0100

    Initial

---
+++ src/main/docker/app/infinity.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+	echo 1
+	sleep 5
+done
---
+++ src/main/java/com/levi9/test/jdhello/App.java
@@ -0,0 +1,14 @@
+package com.levi9.test.jdhello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
